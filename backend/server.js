9178a5b90dca71912d9d76d215fbddd7ea40b38d
Yogesh Suhas Naikwadi <yogeshnaikwadi2000@gmail.com>
2024-01-06 13:57:42 +0530

    ecommerce

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const app = require('./index.js');
+const app = require('./app.js');
 const dotenv=require('dotenv');
 const connectDatabase =require("./config/database");
 const cloudinary = require("cloudinary");
